99321fcf1b5f3aa723d275e0bc370a8a437aa477
LeCapuccino <49007746+LeCapuccino@users.noreply.github.com>
2021-05-18 12:41:19 -0300

    Add files via upload

---
+++ include/point.hpp
@@ -0,0 +1,17 @@
+// header file with point definition
+
+#ifndef POINT_HPP
+#define POINT_HPP
+
+namespace point {
+	class Point {
+	public:
+		float x;
+		float y;
+
+		Point() {}
+		Point(float x_, float y_) : x(x_), y(y_) {}
+	};
+}
+
+#endif
